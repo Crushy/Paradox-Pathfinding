c2adbc5efa393f7ce1b16d1b1185015c953b0779
Pedro Miguel Martins da Silva Caetano <crushyerbones@gmail.com>
2019-10-27 20:15:39 +0200

    Fixed bug where having only one candidate and it being the goal, would lead to the algorithm assuming no path was found.

--- ParadoxPathfinding/PathFinder.cpp
+++ ParadoxPathfinding/PathFinder.cpp
@@ -8,11 +8,15 @@ PathFinder::PathFinder(const SquareGrid& gridRef):
 
 PathFinder::~PathFinder()
 {
-	std::cout << "We are gone :("<<std::endl;
+	std::cout << "Destroying pathfinder"<<std::endl;
 }
 
 std::list<GridLocation>* PathFinder::Pathfind(GridLocation entry, GridLocation goal, HeurFn heurist_func)
 {
+	if (entry == goal) {
+		return 0;
+	}
+
 	std::vector<GridLocation> neighbours = std::vector<GridLocation>(grid.max_neighbours);
 
 	visited = new std::unordered_map<typename GridLocation, VisitedRecord, GridLocation::hash_GridLocation>();
@@ -33,38 +37,46 @@ std::list<GridLocation>* PathFinder::Pathfind(GridLocation entry, GridLocation g
 	CandidateRecord currentNode = startRecord;
 	while (candidates.size() > 0) {
 		currentNode = candidates.top();
-		candidates.pop();
+		
 
 		if (currentNode.coordinate == goal)
 		{
 			break;
 		}
+		else 
 		{
+			candidates.pop();
 			neighbours.clear();
 
-			std::cout << "Visiting " << currentNode.coordinate << std::endl;
+			//std::cout << "Visiting " << currentNode.coordinate << std::endl;
 			grid.GetNeighbours(currentNode.coordinate, neighbours);
 			
 
 			for (auto neighbour : neighbours) {
-				int costToNext = currentNode.costSoFar + 1; //It always costs 1 to move to the neighbour coordinate
+				//Don't consider impassable positions
 				if (grid.GetElement(neighbour) == Impassable) {
 					continue;
 				}
-				else if (visited->count(neighbour) <= 0 || costToNext < (*visited)[neighbour].costSoFar) {
+
+				int costToNext = currentNode.costSoFar + 1; //It always costs 1 to move to the neighbour coordinate
+				
+				//If we aren't a visitor yet or the current path takes less than a previous approach to this node
+				if (visited->count(neighbour) <= 0 || costToNext < (*visited)[neighbour].costSoFar) {
 					(*visited)[neighbour].costSoFar = costToNext;
 					(*visited)[neighbour].coordinate = neighbour;
 					(*visited)[neighbour].previous = currentNode.coordinate;
-
-					candidates.push(CandidateRecord
+					
+					CandidateRecord newRecord = CandidateRecord
 					{
 						neighbour,
 						costToNext,
 						costToNext + heurist_func(neighbour, goal)
-					}
-					);
-				}
+					};
 
+					//Better candidates are prioritized here based on the CandidateRecord "<" operator overload
+					candidates.push(newRecord);
+				}
+				//std::cout << "Visiting neighbour " << neighbour.x << " " << neighbour.y << std::endl;
 			}
 
 		}
@@ -74,23 +86,16 @@ std::list<GridLocation>* PathFinder::Pathfind(GridLocation entry, GridLocation g
 	std::list<GridLocation>* results = new std::list<GridLocation>();
 
 	if (candidates.size() <= 0) {
-		std::cout << -1 << std::endl;
 		return results;
-	}
-	else {
-		//visited[goal].previous = currentNode.coordinate;
 	}
 
 	//Reverse
-
-
 	GridLocation aux = goal;
-
+	//std::cout << "Goal is " << aux << std::endl;
 	
 	do {
 		results->push_front(aux);
-		std::cout << "Going back through " << aux << std::endl;
-		//std::cout << (aux.coordinate) << std::endl;
+		//std::cout << "Going back through " << aux << std::endl;
 		aux = (*visited)[aux].previous;
 		
 	} while (aux != entry);
